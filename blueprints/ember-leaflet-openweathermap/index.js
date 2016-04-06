916a911b818b120a6463199a21d0fbda66b6387d
Alexander Schmitz <arschmitz@gmail.com>
2016-04-06 16:34:19 -0400

    Fix bower package refrence

---
+++ blueprints/ember-leaflet-openweathermap/index.js
@@ -0,0 +1,14 @@
+/*jshint node:true*/
+module.exports = {
+  name: 'ember-leaflet-openweathermap-tile-layer',
+  description: 'openweathermap tile layer',
+
+  normalizeEntityName: function() {
+   // this prevents an error when the entityName is
+   // not specified (since that doesn't actually matter
+   // to us
+  },
+  afterInstall: function(options) {
+   return this.addBowerPackageToProject('git@github.com:buche/leaflet-openweathermap#bb9272b' );
+  }
+};
