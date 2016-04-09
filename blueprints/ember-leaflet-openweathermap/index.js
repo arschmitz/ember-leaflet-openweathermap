d8a3eb4d0e236a130a5f4332e0f769080e68b7cf
Alexander Schmitz <arschmitz@gmail.com>
2016-04-08 22:52:45 -0400

    Blueprint: update bower refrence to fork

--- blueprints/ember-leaflet-openweathermap/index.js
+++ blueprints/ember-leaflet-openweathermap/index.js
@@ -9,6 +9,6 @@ module.exports = {
    // to us
   },
   afterInstall: function(options) {
-   return this.addBowerPackageToProject( 'leaflet-openweathermap', 'git@github.com:buche/leaflet-openweathermap.git#bb9272b' );
+   return this.addBowerPackageToProject( 'leaflet-openweathermap', 'git@github.com:arschmitz/leaflet-openweathermap.git#dc9c836' );
   }
 };
